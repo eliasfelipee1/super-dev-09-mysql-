16d713c83af2e8a4e2f5ab3abdf75d30a5399ebf
elias felipe <uch.finn.tbl@gmail.com>
2026-08-10 21:36:37 -0300

    + comandos e INNER JOIN

--- tabelas_relacionadas.sql
+++ tabelas_relacionadas.sql
@@ -8,9 +8,8 @@ USE biblioteca;
 
 -- Categoria é nx1, pois a categoria tem varios livros
 CREATE TABLE categorias ( 
-    id INT PRIMARY KEY AUTO_INCREMENT, -- chave Primaria (PK) da tabela de categorias
+    id INT PRIMARY KEY AUTO_INCREMENT, 
     nome VARCHAR(50) NOT NULL
-
 );
 
 
@@ -19,37 +18,36 @@ CREATE TABLE livros(
     id INT PRIMARY KEY AUTO_INCREMENT, -- chave primaria (PK)
     nome VARCHAR(100) NOT NULL,
     id_categoria INT NOT NULL,
-    -- chave estrangeira (FK): relaciomento com chave primaria (PK) de outra tabela
-    FOREIGN KEY(id_categoria) references categorias(id)
+    -- chave estrangeira (FK): relacionamento com chave primaria (PK) de outra tabela
+    FOREIGN KEY(id_categoria) REFERENCES categorias(id)
 );
 
 
 INSERT INTO categorias (nome) VALUES
-("critica"), -- 1
-("Comedia"), -- 2
-("Aventura"), -- 3
+("critica"),   -- 1
+("Comedia"),   -- 2
+("Aventura");  -- 3
 
 SELECT id, nome FROM categorias;
-SELECT id, nome, id_categorias FROM livros;
 
 INSERT INTO categorias (nome) VALUES ("Motivacao");
 
-INSERT INTO livros (nome, id_categorias) VALUES
-("Evolucao dos  bichos", 1),
+INSERT INTO livros (nome, id_categoria) VALUES
+("Evolucao dos bichos", 1),
 ("Big Brother e a mentira", 1),
 ("Diario de um banana", 2),
 ("Monica", 2),
 ("Percy Jackson 1", 3),
 ("Habitos Atomicos", 4);
 
-
+SELECT id, nome, id_categoria FROM livros;
 SELECT id, nome FROM categorias;
 
 SELECT
     livros.id,
     livros.nome AS 'livro',
-    categorias.nome AS 'Categoria'
+    categorias.nome AS 'Categoria',
 FROM livros
-INNER JOIN categorias ON ( livros.id_categorias = categorias.id);
+INNER JOIN categorias ON (livros.id_categoria = categorias.id); 
 
 
